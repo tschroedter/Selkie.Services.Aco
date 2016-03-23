c4b3a1e4db3167b499b74633691bf9544212ae2e
thomas <thomas.schroedter@gmx.net>
2016-03-23 21:47:23 +1100

    Update from private repository - Selkie services are now Windows services - Added MSI setups for windows services - Added support for separate turn radius for port and starboard

---
+++ Selkie.Services.Aco.Windows.Service/AcoServiceMain.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics.CodeAnalysis;
+using Castle.Windsor.Installer;
+using Selkie.Services.Common;
+
+namespace Selkie.Services.Aco.Windows.Service
+{
+    [ExcludeFromCodeCoverage]
+    public class AcoServiceMain : ServiceMain
+    {
+        public void Run()
+        {
+            StartServiceAndRunForever(FromAssembly.This(),
+                                      AcoService.ServiceName);
+        }
+    }
+}
